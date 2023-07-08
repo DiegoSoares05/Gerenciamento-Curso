2fc83371d76ca14161c0135dee7c56eb4a614265
Diego soares de carvalho Junior <dieguinhosoares2005@gmail.com>
2023-07-08 19:28:29 -0300

    primeiro commit

---
+++ Gerenciamento.c
@@ -0,0 +1,615 @@
+/*********************************************************************/
+/**   ACH2001 - Introducao a Programacao                            **/
+/**   EACH-USP - Primeiro Semestre de 2023                          **/
+/**   <turma> - <nome do professor>                                 **/
+/**                                                                 **/
+/**   Segundo Exercicio-Programa                                    **/
+/**                                                                 **/
+/**   Diego soares de carvalho Junior            14652571           **/
+/**                                                                 **/
+/*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+#define MAX_TURMAS 20
+#define DISCIPLINAS_NECESSARIAS 5
+#define MAX_ALUNOS_CURSO 100
+#define MAX_TURMAS_CURSO 50
+
+typedef int bool;
+
+/*
+TURMA: tem por objetivo representar uma turma. E uma estrutura que possui cinco
+campos: codigoDisciplina do tipo inteiro que corresponde ao codigo numerico da
+disciplina, nomeDisciplina do tipo ponteiro para caracteres que aponta para o
+nome da disciplina; totalVagas do tipo inteiro que contem o numero de vagas
+oferecidas na turma; numAlunos do tipo inteiro que contem o numero atual de
+alunos na turma; e nusps do tipo ponteiro para inteiro que aponta para um
+arranjo que contem o numero USP de cada um dos alunos da turma.
+*/
+typedef struct {
+  int codigoDisciplina;
+  char *nomeDisciplina;
+  int totalVagas;
+  int *nusps;
+  int numAlunos;
+} TURMA;
+
+/*
+DESEMPENHO: tem por objetivo representar o desempenho de um aluno em uma turma.
+Possui quatro campos: turma que e do tipo ponteiro para TURMA e aponta para a
+turma a que o desempenho se refere; nota do tipo inteiro que contem a nota do
+aluno na respectiva turma (de 0 [zero] a 100); frequencia do tipo inteiro que
+contem a frequencia do aluno na respectiva turma (de 0 [zero] a 100); e status
+do tipo char cujo valor sera 'M' para o aluno recem matriculado na turma e
+podera ter outros dois valores possiveis dependendo da nota e da frequencia do
+aluno ('A' para aprovado e 'R' para reprovado).
+*/
+typedef struct {
+  TURMA *turma;
+  int nota;
+  int frequencia;
+  char status;
+} DESEMPENHO;
+
+/*
+ALUNO: tem por objetivo representar um aluno e seu historico escolar. Possui
+cinco campos: nome do tipo ponteiro para caracteres que aponta para o nome do
+aluno; nusp do tipo inteiro que representa o numero USP do aluno; historico do
+tipo arranjo de DESEMPENHO que corresponde ao historico escolar do aluno, com
+suas matriculas e desempenho nas turmas; turmasNoHistorico do tipo inteiro que
+indica em quantas turmas o aluno ja se matriculou; e status do tipo char cujo
+valor sera 'M' para o aluno que ainda nao conclui seu curso e podera ter outros
+dois valores possiveis dependendo de seu desempenho no conjunto de turmas ('F'
+para formado e 'J' para jubilado).
+*/
+typedef struct {
+  char *nome;
+  int nusp;
+  DESEMPENHO historico[MAX_TURMAS];
+  int turmasNoHistorico;
+  char status;
+} ALUNO;
+
+/*
+CURSO: tem por objetivo representar um curso que e composto por turmas e alunos.
+Possui quatro campos: alunos do tipo arranjo de ponteiros para ALUNOs; numAlunos
+do tipo inteiro que armazena a quantidade de alunos no curso; turma do tipo
+arranjo de ponteiros para TURMAs; e numTurmas do tipo inteiro que armazena a
+quantidade de turmas no curso.
+*/
+typedef struct {
+  ALUNO *alunos[MAX_ALUNOS_CURSO];
+  int numAlunos;
+  TURMA *turmas[MAX_TURMAS_CURSO];
+  int numTurmas;
+} CURSO;
+
+/******** FUNCOES PARA CRIAR ALUNOS, DESEMPENHOS, TURMAS OU CURSOS ********/
+
+/* Funcao para a criacao de um novo ALUNO.
+   A funcao preenche todos os valores de seus campos e retorna esse novo ALUNO.
+*/
+ALUNO novoAluno(char *nome, int nusp) {
+  ALUNO novo;
+  novo.nome = nome;
+  novo.nusp = nusp;
+  novo.turmasNoHistorico = 0;
+  novo.status = 'M';
+  return novo;
+}
+
+/* Funcao para a criacao de uma nova turma.
+   A funcao preenche os valores de seus campos e retorna essa nova turma.
+*/
+TURMA novaTurma(int codigoDisciplina, char *nomeDisciplina, int totalVagas) {
+  TURMA nova;
+  nova.codigoDisciplina = codigoDisciplina;
+  nova.nomeDisciplina = nomeDisciplina;
+  nova.nusps = (int *)malloc(sizeof(int) * totalVagas);
+  nova.totalVagas = totalVagas;
+  nova.numAlunos = 0;
+  return nova;
+}
+
+/* Funcao para a criacao de um novo curso.
+   A funcao preenche os valores de seus campos e retorna esse novo curso.
+*/
+CURSO novoCurso() {
+  CURSO novo;
+  novo.numAlunos = 0;
+  novo.numTurmas = 0;
+  return novo;
+}
+
+/* Funcao para a criacao de um novo desempenho.
+   A funcao preenche os valores de seus campos e retorna esse novo desempenho.
+*/
+DESEMPENHO novoDesempenho(TURMA *turma) {
+  DESEMPENHO novo;
+  novo.turma = turma;
+  novo.nota = 0;
+  novo.frequencia = 0;
+  novo.status = 'M';
+  return novo;
+}
+
+/******** FUNCOES QUE VOCE DEVERA IMPLEMENTAR/COMPLETAR *******************/
+
+bool adicionarAlunoAoCurso(ALUNO *aluno, CURSO *curso) {
+
+  if (aluno == NULL || curso == NULL) {
+    return false;
+  }
+  if (curso->numAlunos == MAX_ALUNOS_CURSO) {
+    return false;
+  }
+  for (int i = 0; i < curso->numAlunos; i++) {
+    if (curso->alunos[i]->nusp == aluno->nusp) {
+      return false;
+    }
+  }
+  curso->alunos[curso->numAlunos] = aluno;
+  curso->numAlunos++;
+  return true;
+}
+////////////////////////////////////////////
+
+bool adicionarTurmaAoCurso(TURMA *turma, CURSO *curso) {
+
+  if (turma == NULL || curso == NULL) {
+    return false;
+  }
+  if (curso->numTurmas == MAX_TURMAS_CURSO) {
+    return false;
+  }
+  for (int i = 0; i < curso->numTurmas; i++) {
+    if (curso->turmas[i] == turma) {
+      return false;
+    }
+  }
+  curso->turmas[curso->numTurmas] = turma;
+  curso->numTurmas++;
+  return true;
+}
+////////////////////////////////////////////////////////////////
+
+bool cadastrarNota(ALUNO *aluno, int codigoDisciplina, int nota, int freq) {
+
+  if (aluno == NULL || nota < 0 || nota > 100 || freq < 0 || freq > 100) {
+    return 0;
+  }
+  // verificando se aluno não possui historico nessa discipina 
+  for (int i = 0; i < aluno->turmasNoHistorico; i++) {
+    if (aluno->historico[i].turma->codigoDisciplina == codigoDisciplina &&
+        aluno->historico[i].turma == NULL &&
+        aluno->historico[i].status == 'M') {
+      return false;
+    } // efetuando o cadastro das notas e verificando status
+    else{
+      aluno->historico[i].frequencia = freq;
+      aluno->historico[i].nota = nota;
+
+      if (nota >= 50 && freq >= 70) {
+        aluno->historico[i].status = 'A';
+      }else {
+        aluno->historico[i].status = 'R';
+      }
+     
+    }
+  }
+  
+    
+  return true;
+}
+
+//////////////////////////////////////////////////////////
+
+void atualizarStatusAluno(ALUNO *aluno) {
+
+  int aprov = 0; // numero de aprovações
+  int repro = 0; // numero de reprovações
+
+  for (int i = 0; i < aluno->turmasNoHistorico;
+       i++) { // percorre o historico do aluno para ver o desempenho em cada
+              // matéria, se for 'A' eu adiciono uma aprovação, se for 'R' eu
+              // adiciono uma reprovação
+
+    if (aluno->historico[i].status == 'A') { // atualiza o valor de aprovações
+      aprov++;
+    } else if (aluno->historico[i].status ==
+               'R') { // atualiza o valor de reprovações
+      repro++;
+    }
+  }
+  if (aprov >= DISCIPLINAS_NECESSARIAS) { // aluno formado
+    aluno->status = 'F';
+
+  } else if ((MAX_TURMAS - repro) < DISCIPLINAS_NECESSARIAS) {
+    aluno->status = 'J'; // aluno jubilado
+  } else {
+    aluno->status = 'M'; // aluno continua matriculado
+  }
+}
+
+//////////////////////////MATRICULA////////////////////////////
+
+bool matricularAlunoEmTurma(ALUNO *aluno, TURMA *turma) {
+
+  // efetuando as verificações
+
+  if (aluno == NULL || turma == NULL) {
+    return false;
+  }
+
+  // chamando função que atualiza o status do aluno, com esse aluno como
+  // parametro
+  atualizarStatusAluno(aluno);
+
+  if (turma->numAlunos == turma->totalVagas || aluno->status != 'M' ||
+      aluno->turmasNoHistorico == MAX_TURMAS) {
+    return false;
+  }
+
+  // verificando se aluno está matriculado na turma
+  for (int i = 0; i < turma->numAlunos; i++) {
+    if (aluno->nusp == turma->nusps[i]) {
+      return false;
+    }
+  }
+
+  // verificando se está matriculado em outra turma da mesma disciplina ou ja
+  // foi aprovado ou está matriculado
+
+  for (int i = 0; i < aluno->turmasNoHistorico; i++) {
+    if (aluno->historico[i].turma->codigoDisciplina ==
+            turma->codigoDisciplina &&
+        (aluno->historico[i].status == 'M' ||
+         aluno->historico[i].status == 'A')) {
+      return false;
+    }
+  }
+
+  //// matriculando novo aluno
+  aluno->historico[aluno->turmasNoHistorico] = novoDesempenho(turma);
+  aluno->turmasNoHistorico += 1;
+  turma->nusps[turma->numAlunos] = aluno->nusp;
+  turma->numAlunos++;
+  return true;
+}
+
+/****** FUNCOES PARA EXIBIR DADOS DO CURSO, DAS TURMAS OU DOS ALUNOS ******/
+
+/*
+Funcao para imprimir informacoes detalhadas de um aluno.
+*/
+void imprimirDadosAluno(ALUNO *a1) {
+  if (!a1)
+    return;
+  printf("**************************************************************\n");
+  printf("Imprimindo informacoes do aluno: %i - %s.\n", a1->nusp, a1->nome);
+  printf("Status: %c\n", a1->status);
+  int d;
+  DESEMPENHO temp;
+  printf("Turmas cursadas: %i\n", a1->turmasNoHistorico);
+  for (d = 0; d < a1->turmasNoHistorico; d++) {
+    temp = a1->historico[d];
+    printf("\tDisciplina: %s, Nota: %i, Frequencia: %i, Status: %c\n",
+           temp.turma->nomeDisciplina, temp.nota, temp.frequencia, temp.status);
+  }
+}
+
+/*
+Funcao para imprimir informacoes detalhadas de uma turma.
+*/
+void imprimirDadosTurma(TURMA *t1) {
+  if (!t1)
+    return;
+  printf("**************************************************************\n");
+  printf("Imprimindo informacoes da turma: %i - %s.\n", t1->codigoDisciplina,
+         t1->nomeDisciplina);
+  int a;
+  printf("A turma contem %i vagas e %i alunos:\n", t1->totalVagas,
+         t1->numAlunos);
+  for (a = 0; a < t1->numAlunos; a++) {
+    printf("\t%i\n", t1->nusps[a]);
+  }
+}
+
+/*
+Funcao para imprimir informacoes gerais sobre um curso.
+*/
+void imprimirDadosCurso(CURSO c1) {
+  printf("\n###################################################################"
+         "################\n");
+  printf("Imprimindo informacoes do curso.\n");
+  printf("Ha %i alunos(s) neste curso.\n", c1.numAlunos);
+  ALUNO *aluno;
+  for (int a = 0; a < c1.numAlunos; a++) {
+    aluno = c1.alunos[a];
+    printf("Aluno: %s\tnusp: %i\n", aluno->nome, aluno->nusp);
+  }
+  printf("\n");
+  TURMA *turma;
+  printf("Ha %i turmas(s) neste curso.\n", c1.numTurmas);
+  for (int t = 0; t < c1.numTurmas; t++) {
+    turma = c1.turmas[t];
+    printf("Turma: %s\tvagas: %i\tmatriculados: %i\n", turma->nomeDisciplina,
+           turma->totalVagas, turma->numAlunos);
+  }
+  printf("#####################################################################"
+         "##############\n");
+}
+
+/*
+Funcao para imprimir informacoes detalhadas de todos os alunos de um curso.
+*/
+void imprimirDadosTodosAlunos(CURSO c1) {
+  printf("\n###################################################################"
+         "################\n");
+  printf("Imprimindo informacoes de todos os turmas do curso.\n");
+  printf("Ha %i alunos(s) neste curso.\n", c1.numAlunos);
+  for (int a = 0; a < c1.numAlunos; a++) {
+    imprimirDadosAluno(c1.alunos[a]);
+  }
+  printf("#####################################################################"
+         "##############\n");
+}
+
+/*
+Funcao para imprimir informacoes detalhadas de todas as turmas de um curso.
+*/
+void imprimirDadosTodasTurmas(CURSO c1) {
+  printf("\n###################################################################"
+         "################\n");
+  printf("Imprimindo informacoes de todos as turmas do curso.\n");
+  printf("Ha %i turma(s) neste curso.\n", c1.numTurmas);
+  for (int t = 0; t < c1.numTurmas; t++) {
+    imprimirDadosTurma(c1.turmas[t]);
+  }
+  printf("#####################################################################"
+         "##############\n");
+}
+
+/*** FUNCAO MAIN QUE REALIZA ALGUNS TESTES SOBRE AS FUNCOES IMPLMENTADAS ***/
+
+int main() {
+
+  printf("ATENCAO: Nem todas as caracteristicas do enunciado sao testadas "
+         "neste exemplo.\n");
+  printf("         Cabe a cada aluno(a) testar cuidadosamente seu EP.\n\n");
+
+  printf("*** Criando alunos *******************************************\n");
+  ALUNO a1 = novoAluno("Aluno A\0", 22222);
+  ALUNO a2 = novoAluno("Aluno B\0", 33333);
+  ALUNO a3 = novoAluno("Aluno C\0", 44444);
+  ALUNO a4 = novoAluno("Aluno D\0", 55555);
+  ALUNO a5 = novoAluno("Aluno C\0", 44444);
+  ALUNO a6 = novoAluno("Aluno E\0", 66666);
+  ALUNO a7 = novoAluno("Diego \0", 14655);
+
+  printf("*** Criando turmas **************************************\n");
+  TURMA t1 = novaTurma(2001, "Introducao a Programacao\0", 3);
+  TURMA t2 = novaTurma(2002, "Introducao a Analise de Algoritmos\0", 4);
+  TURMA t3 = novaTurma(2023, "Algoritmos e Estruturas de Dados I\0", 5);
+  TURMA t4 = novaTurma(2024, "Algoritmos e Estruturas de Dados II\0", 6);
+  TURMA t5 = novaTurma(2016, "Inteligencia Artificial\0", 7);
+  TURMA t6 = novaTurma(2023, "Algoritmos e Estruturas de Dados I\0", 8);
+
+  printf("*** Criando um curso *******************************************\n");
+  CURSO meuCurso = novoCurso();
+
+  imprimirDadosCurso(meuCurso);
+
+  printf("*** Inserindo alunos no curso "
+         "*******************************************\n");
+
+  if (adicionarAlunoAoCurso(&a1, &meuCurso))
+    printf("1. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarAlunoAoCurso(&a2, &meuCurso))
+    printf("2. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarAlunoAoCurso(&a3, &meuCurso))
+    printf("3. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarAlunoAoCurso(&a4, &meuCurso))
+    printf("4. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarAlunoAoCurso(&a5, &meuCurso))
+    printf("NOK\n");
+  else
+    printf("5. OK\n");
+  if (adicionarAlunoAoCurso(&a6, &meuCurso))
+    printf("6. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarAlunoAoCurso(&a4, &meuCurso))
+    printf("NOK\n");
+  else
+    printf("7. OK\n");
+
+  imprimirDadosCurso(meuCurso);
+
+  printf("*** Inserindo turmas no curso "
+         "*******************************************\n");
+
+  if (adicionarTurmaAoCurso(&t1, &meuCurso))
+    printf("8. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t2, &meuCurso))
+    printf("9. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t3, &meuCurso))
+    printf("10. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t4, &meuCurso))
+    printf("11. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t5, &meuCurso))
+    printf("12. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t6, &meuCurso))
+    printf("13. OK\n");
+  else
+    printf("NOK\n");
+  if (adicionarTurmaAoCurso(&t3, &meuCurso))
+    printf("NOK\n");
+  else
+    printf("14. OK\n");
+
+  imprimirDadosCurso(meuCurso);
+  imprimirDadosTodosAlunos(meuCurso);
+  imprimirDadosTodasTurmas(meuCurso);
+
+  printf("\n\n*** Matriculando alunos nas turmas "
+         "**************************************\n");
+  if (matricularAlunoEmTurma(&a7, &t6))
+    printf("15. OK\n");
+
+  if (matricularAlunoEmTurma(&a1, &t1))
+    printf("15. OK\n");
+  else
+    printf("NOK\n");
+
+  if (matricularAlunoEmTurma(&a1, &t2))
+    printf("16. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a1, &t3))
+    printf("17. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a3, &t2))
+    printf("18. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a2, &t1))
+    printf("19. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a2, &t2))
+    printf("20. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a3, &t1))
+    printf("21. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a4, &t6))
+    printf("22. OK\n");
+  else
+    printf("NOK\n");
+
+  if (matricularAlunoEmTurma(&a4, &t1))
+    printf("15. OK\n");
+  else
+    printf("NOK\n");
+
+  imprimirDadosCurso(meuCurso);
+  imprimirDadosTodosAlunos(meuCurso);
+  imprimirDadosTodasTurmas(meuCurso);
+
+  printf("\n\n*** Cadastrando notas "
+         "***************************************************\n");
+
+  if (cadastrarNota(&a7, t6.codigoDisciplina, 100, 90))
+    printf("23. OK\n");
+  else
+    printf("NOK\n");
+
+  if (cadastrarNota(&a1, t1.codigoDisciplina, 100, 90))
+    printf("23. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t2.codigoDisciplina, 70, 70))
+    printf("24. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t3.codigoDisciplina, 40, 75))
+    printf("25. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t2.codigoDisciplina, 80, 80))
+    printf("26. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t4.codigoDisciplina, 90, 90))
+    printf("27. OK\n");
+  else
+    printf("NOK\n");
+
+  imprimirDadosAluno(&a1);
+
+  printf("\n\n*** Matriculando alunos nas turmas (novamente) "
+         "**************************\n");
+  if (matricularAlunoEmTurma(&a2, &t3))
+    printf("28. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a1, &t4))
+    printf("29. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a1, &t5))
+    printf("30. OK\n");
+  else
+    printf("NOK\n");
+  if (matricularAlunoEmTurma(&a3, &t6))
+    printf("31. OK\n");
+  else
+    printf("NOK\n");
+
+  imprimirDadosAluno(&a1);
+
+  printf("\n\n*** Cadastrando notas (novamente) "
+         "***************************************\n");
+  if (cadastrarNota(&a1, t4.codigoDisciplina, 100, 90))
+    printf("32. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t5.codigoDisciplina, 70, 70))
+    printf("33. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t6.codigoDisciplina, 60, 100))
+    printf("34. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t3.codigoDisciplina, 80, 80))
+    printf("35. OK\n");
+  else
+    printf("NOK\n");
+  if (cadastrarNota(&a1, t4.codigoDisciplina, 90, 90))
+    printf("36. OK\n");
+  else
+    printf("NOK\n");
+
+  imprimirDadosAluno(&a1);
+
+  imprimirDadosAluno(&a7);
+
+  imprimirDadosCurso(meuCurso);
+  imprimirDadosTodosAlunos(meuCurso);
+  imprimirDadosTodasTurmas(meuCurso);
+
+  printf("\n\n*** Atualizando status "
+         "**************************************************\n");
+  atualizarStatusAluno(&a1);
+  imprimirDadosAluno(&a1);
+
+  return 0;
+}
